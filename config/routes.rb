178f2946746c59bfc1794afa972b314c123663f4
Тимур Ким <kepaliga@mail.ru>
2025-10-25 11:59:37 +0000

    added rswag examples

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'
-
   namespace :api do
     namespace :v1 do
       resources :complexity_jobs, only: [:show, :create], path: 'complexity-score', param: :job_id
     end
   end
+
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
 end
